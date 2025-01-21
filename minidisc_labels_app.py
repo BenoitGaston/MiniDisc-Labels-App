740607794c19ed66cfaf1eefb0054e2b052c997e
BenGast <Sialcant@yahoo.fr>
2025-01-21 14:47:35 +0100

    refact

--- minidisc_labels_app.py
+++ minidisc_labels_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-from  src.audio_libs_tlk.lib_scan import scan_and_process
+from  .create_md_labels import MiniDiscCovers
 
 from pathlib import Path
 
@@ -12,13 +12,13 @@ def main():
         # Additional string arguments
         path_to_library_data = st.text_input("Path to a directory containing a .m3u8 playlist containing the string MiniDisc in its name).", value="")
         # Folder path input
-        create_minidisc_labels = True
+
         
         # Run button
         if st.button("Run"):
-            scan_and_process(path_to_library_data=path_to_library_data,
-            create_minidisc_labels = create_minidisc_labels,)
 
+                    minidisc_covers = MiniDiscCovers(path_to_music_folder=path_to_library_data)
+                    minidisc_covers.build_md_labels()
             
             st.success(f"Your MiniDisc Labels have been created and are available under this path: {Path(path_to_library_data)}/MiniDisc-Labels")
             st.write("After reviewing the svg files created, you might find necessary to modify (in particular shorten/abreviate) some Artist and Album names. You might also want to update the text and background colomns label by label")
